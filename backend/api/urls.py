e44ae29b13613157ed13457454110116af1951e6
Mufil Rahman A <mufil.r@simplicontract.com>
2025-03-15 22:15:49 +0530

    Add Note model, serializers, and CRUD endpoints for notes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,12 @@
+#backend/api/urls.py
 from django.urls import path
-from api.views import CreateUserView
+from api.views import CreateUserView , NoteListCreate , NoteDelete
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('user/register/', CreateUserView.as_view(), name='register'),
     path('token/', TokenObtainPairView.as_view(), name='get_token'),
     path('token/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
+    path('note/', NoteListCreate.as_view(), name='note_list'),
+    path('note/delete/<int:pk>/', NoteDelete.as_view(), name='note_delete'),
 ]
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,3 +1,4 @@
+#backend/urls.py
 from django.contrib import admin
 from django.urls import path
 from django.urls import path , include
