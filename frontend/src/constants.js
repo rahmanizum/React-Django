49e126bbf8db48e31adae6e8291a5153d02645d5
Mufil Rahman A <mufil.r@simplicontract.com>
2025-03-20 19:19:19 +0530

    Add note retrieval and deletion functionality in Home component; introduce CSS for improved layout

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -5,4 +5,6 @@ export const API_ENDPOINT = {
     GET_TOKEN_BY_REFRESH: "/api/token/refresh/",
     GET_TOKEN: "/api/token/",
     REGISTER: "/api/user/register/",
+    GET_NOTE: "/api/note/",
+    DELETE_NOTE: "note/delete/",
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,58 @@
 import React from 'react'
-
+import { useState , useEffect } from 'react'
+import api from '../api'
+import { API_ENDPOINT } from '../constants'
+import "../../src/styles/home.css"
 const Home = () => {
+
+  const [notes, setNotes] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [content, setContent] = useState("")
+  const [title, setTitle] = useState("")
+
+  useEffect(() => {
+    getNotes()
+  }
+  , [])
+
+  const getNotes = async () => {
+    setLoading(true)
+    try {
+      const response = await api.get(API_ENDPOINT.GET_NOTE)
+      setNotes(response.data)
+    } catch (error) {
+      alert("Error")
+      console.log(error)
+    }
+    setLoading(false)
+  }
+
+  const deleteNote = async (id) => {
+    try {
+      await api.delete(`${API_ENDPOINT.DELETE_NOTE}${id}`);
+      getNotes();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div>Home</div>
+    <div className="container">
+      <h1>Notes</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="notes-list">
+          {notes.map((note) => (
+            <div key={note.id} className="note-card">
+              <h2>{note.title}</h2>
+              <p>{note.content}</p>
+              <button className="delete-btn" onClick={() => deleteNote(note.id)}>Delete</button>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
   )
 }
 
